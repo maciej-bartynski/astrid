f0d581a524235678c95007533e5f5378af44f0c7
Maciej Bartyński <maciek.bartynski@gmail.com>
2019-04-07 19:55:54 +0200

    Updates

---
+++ precache-manifest.3a32c3cd9cac63a5f0df331792cdcaed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "96629eec6c45767a8efd",
+    "url": "/astrid/static/js/runtime~main.3dc0ba86.js"
+  },
+  {
+    "revision": "2b932fcee309fae6dc25",
+    "url": "/astrid/static/js/main.e362833d.chunk.js"
+  },
+  {
+    "revision": "6bb5e72d7b5c4615e606",
+    "url": "/astrid/static/js/2.2755ab56.chunk.js"
+  },
+  {
+    "revision": "2b932fcee309fae6dc25",
+    "url": "/astrid/static/css/main.f92f9ed9.chunk.css"
+  },
+  {
+    "revision": "c5fce9031e56edb8aff359f99f20244e",
+    "url": "/astrid/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/astrid/precache-manifest.3a32c3cd9cac63a5f0df331792cdcaed.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/astrid/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
